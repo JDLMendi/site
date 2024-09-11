d98528228f2db2ab9358a1e07746cd2e07bef7e8
Jhundon De Leon Mendi <jhundon.mendi21@gmail.com>
2024-09-11 10:57:37 +0100

    please

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jdlmendi.github.io',
+    site: 'https://jdlmendi.github.io/site',
     base: ''
 });
